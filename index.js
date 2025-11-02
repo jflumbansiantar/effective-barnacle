271d110c6a9774f992d8301e5b3accf063be8076
Zullss <jflumbansiantar@gmail.com>
2025-11-02 23:09:01 +0700

    add bootstrap to project

--- index.js
+++ index.js
@@ -2,24 +2,9 @@ function changeContent(page) {
 	var contentDiv = document.getElementById('side-content');
 	contentDiv.style.display='block';
 	switch (page) {
-		case 'home':
-			contentDiv.innerHTML = `
-				<img src=
-"https://media.geeksforgeeks.org/wp-content/uploads/geeksforgeeks-12.png">
-				<h2>
-					Welcome to the Home Page!
-				</h2>
-				<p>
-					This is the main page of our SPA.
-				</p>
-				<p>
-					Explore the different sections using
-					the navigation menu.
-				</p>
-			`;
-			break;
 		case 'about':
 			contentDiv.innerHTML = `
+			
 				<h2>Halooooo</h2>
 				<p>
 					Kenalin , Saya Fran!.
@@ -40,38 +25,78 @@ function changeContent(page) {
 			contentDiv.innerHTML = 
 				`<h2>Proyek</h2> 
 				<p>
-					Disini saya cantumin 3 proyek terakhir saja yaa
+					Beberapa proyek yang pernah saya kerjakan :)
 				</p> 
-				<div id='project1'>
-					<h3>Sales4U Maybank Indonesia</h3>
-					<p>Role sebagai Backend Developer (Java)</p>
-					<p>Tanggungjawab:
-						<ul>
-							<li>Mengembangkan aplikasi sesuai dengan kebutuhan bisnis yang mengacu pada user story dan desain yang disepakati</li>
-							<li>Bekerja sama secara erat dengan anggota tim IT lainnya dan pengguna dalam melakukan SIT (System Integration Test) dan UAT (User Acceptance Test)</li>
-							<li>Bertanggung jawab penuh atas masalah aplikasi yang muncul dengan melakukan investigasi terhadap masalah aplikasi melalui Log Database, Debugging, dan Mendefinisikan masalah apakah data atau aplikasi yang terpengaruh dan melakukan perbaikan</li>
-						</ul>
+				<div class="row grid text-center">
+					<div id='project1' class="card project ">
+						<h3 class="card-header">Sales Apps Project</h3>
+						<div class="card-body">
+							<h6 class="card-title">
+								Klien : Maybank Indonesia <p/>
+								Role sebagai Backend Developer (Java)<p/>
+							</h6>
+						</div>
+						<div class="card-footer">
+							<ul>
+								<li>Java Springboot</li>
+								<li>JSP</li>
+								<li>NodeJS/ExpressJS</li>
+								<li>ReactJS</li>
+								<li>React Native</li>
+								<li>OracleDB</li>
+							</ul>
+						</div>
+					</div>
+					<div id='project2' class='card project'> 
+						<h3 class="card-header">Protelindo Dashboard Project</h3>
+						<div class="card-body">
+							<h6 class="card-title">
+								Klien : Protelindo <p/>
+								Role sebagai FullStack Developer  (ASP.NET)<p/>
+							</h6>
+						</div>
+						<div class="card-footer">
+							<ul>
+								<li>ASP.NET</li>
+								<li>PostgreSQL</li>
+								<li>Microsoft SQL Server</li>
+							</ul>
+						</div>
+					</div>
+					<div id='project3' class='card project'>
+						<h3 class="card-header">Credit Policy System Project </h3>
+						<div class="card-body">
+							<h6 class="card-title">
+								Klien : Maybank Indonesia <p/>
+								Role sebagai FullStack Developer  (Java)<p/>
+							</h6>
+						</div>
+						<div class="card-footer">
+							<ul>
+								<li>Java Springboot</li>
+								<li>JSP</li>
+								<li>OracleDB</li>
+							</ul>
+						</div>
+					</div>
+					<div id='project4' class='card project'>
+						<h3 class="card-header">ERP Project </h3>
+						<div class="card-body">
+							<h6 class="card-title">
+								Klien : MING <p/>
+								Role sebagai Supervisor IT <p/>
+							</h6>
+						</div>
+						<div class="card-footer">
+							<ul>
+								<li>Python (Frappe)</li>
+								<li>HTML</li>
+								<li>MariaDB</li>
+							</ul>
+						</div>
+					</div>
 				</div>
-				<div id='project1'>
-					<h3>Protelindo Dashboard Project</h3>
-					<p>Role sebagai FullStack Developer (.NET)</p>
-					<p>Tanggungjawab:
-						<ul>
-							<li>Mengembangkan aplikasi sesuai dengan kebutuhan bisnis yang mengacu pada user dokumen spesifikasi fungsional yang disepakati</li>
-							<li>Bekerja sama secara erat dengan anggota tim IT lainnya dan pengguna dalam melakukan SIT (System Integration Test) dan UAT (User Acceptance Test)</li>
-							<li>Bertanggung jawab penuh atas masalah aplikasi yang muncul dengan melakukan investigasi terhadap masalah aplikasi melalui Log Database, Debugging, dan Mendefinisikan masalah apakah data atau aplikasi yang terpengaruh dan melakukan perbaikan</li>
-						</ul>
-				</div>
-				<div id='project1'>
-					<h3>Credit Policy System Project Maybank Indonesia </h3>
-					<p>Role sebagai FullStack Developer (Java)</p>
-					<p>Tanggungjawab:
-						<ul>
-							<li>Mengembangkan aplikasi sesuai dengan kebutuhan bisnis yang mengacu pada user dokumen spesifikasi fungsional yang disepakati</li>
-							<li>Bekerja sama secara erat dengan anggota tim IT lainnya dan pengguna dalam melakukan SIT (System Integration Test) dan UAT (User Acceptance Test)</li>
-							<li>Bertanggung jawab penuh atas masalah aplikasi yang muncul dengan melakukan investigasi terhadap masalah aplikasi melalui Log Database, Debugging, dan Mendefinisikan masalah apakah data atau aplikasi yang terpengaruh dan melakukan perbaikan</li>
-						</ul>
-				</div>
+				
 				`;
 			break;
 
